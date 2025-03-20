76e52d457e2e02fbc9b55e08c707019bf3975a51
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-03-20 12:28:44 +0530

    Update Square.js

--- Day4/Square.js
+++ Day4/Square.js
@@ -1,10 +1,15 @@
-const http=require('http');
-http.createServer(req,res)=>{
-if(req.url === '/favicon.ico'){
-}
-  else{
-    var num=req.url.slice(req.url.indexof("=")+1,req.url.length-1)
-    square=num*num;
-    res.write("square of ",num,"is",square);
-  }
-}).listen('4000');
+ const http = require('http');
+
+http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') {
+        
+    }
+
+    else {
+        let num = req.url.slice(req.url.indexOf("=") + 1);
+        let square = num * num;
+
+        res.write(`Square of ${num} is ${square}`);
+        res.end(`Square of ${num} is ${square}`);
+    }
+}).listen(4000);
