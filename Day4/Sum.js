095360fc1f518aa34ba74a189d6497a3c89b9b5d
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-03-20 14:28:11 +0530

    Update Sum.js

--- Day4/Sum.js
+++ Day4/Sum.js
@@ -1,15 +1,19 @@
-   const http = require('http');
- 
+ const http = require('http');
+const sum=require('./sum');
 http.createServer((req, res) => {
     if (req.url === '/favicon.ico') {
         
     }
 
-    else {
-        let num1 = req.url.slice(req.url.indexOf("=") + 1,req.url.indexof("&")-1);
-      let num2=req.url.slice(req.url.
-        
-        res.write(result);
-        res.end(result);
-    }
+    
+    let num1 = parseInt(req.url.slice(req.url.indexOf("=") + 1, req.url.indexOf("&")));
+    let num2 = parseInt(req.url.slice(req.url.lastIndexOf("=") + 1));
+
+    
+    
+    let result =sum.sum(10,20)
+
+    
+    res.write("sum :",result);
+    res.end(`Result: ${result}`);
 }).listen(4000);
