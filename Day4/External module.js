bfce8e48519cb13eba356fce43266ad8d0b31fbb
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-03-20 11:52:23 +0530

    Create External module.js

---
+++ Day4/External module.js
@@ -0,0 +1,8 @@
+const sum=require('sum-of-digits');
+console.log("sum:",sumOfDigits(31));
+//https://www.npmjs.com/package/sum-of-digits
+//npm i sum-of-digits
+const dig=require('sum-of-two-numbers');
+console.log("sum digit:",sum(10,20));
+//https://www.npmjs.com/package/sum-of-two-numbers
+//npm install sum-of-two-numbers
