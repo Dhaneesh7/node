cf6ab182ff09af1483d334afe9c585df33e6dfec
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-04-02 10:50:46 +0530

    Create studentmanager.js

---
+++ Day4/studentmanager.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const app=express();
+const port=3000;
+
+let users=[
+{
+    name:sonu,
+    age:22,
+    mark:33
+}
+];
+
+app.get("/getuser",(req,res)=>{
+  
+    
+})
+app.post("/getuser",(req,res)=>{
+    
+})
+app.delete("/deleteuser",(req,res)=>{
+    
+})
+
+app.listen(4000)
