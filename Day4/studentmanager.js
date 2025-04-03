88140b109dae2afdaf217b97ef7e8f61653f2aec
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-04-03 14:12:47 +0530

    Update studentmanager.js

--- Day4/studentmanager.js
+++ Day4/studentmanager.js
@@ -2,31 +2,40 @@ const express=require("express");
 const app=express();
 const port=3000;
 
-let users=[
+let students=[
 {
     name:sonu,
     age:22,
-    mark:33
+    mark:33,
+    email:sonu9@gmail.com
 }
 ];
 
 app.get("/getuser",(req,res)=>{
   
-    res.json(users)
+    res.json(students)
 });
 app.post("/getuser",(req,res)=>{
 const newUser = req.body;
     users.push(newUser);
-    res.json({message: "user added:",newUser})
+    res.json({message: "student added:",newUser})
 })
 app.delete("/deleteuser",(req,res)=>{
-const name=req.body;
-let index=users.Indexof(name);
+const id=req.body;
+let index=users.Indexof(id);
 if(index==-1){
 users.splice(index,1);
-res.json({message: "user deleted:"})
+res.json({message: "student deleted:"})
+}
+else{
+
+res.json({message: "student doesn't found"})
 }
     
 })
+app.delete("/deletealluser",(req,res)=>{
+  users=[];
 
+    res.json({message: "all users deleted:"})
+});
 app.listen(4000)
