219727a16bd8d61be7f86dc6dc07ed0b300b1de1
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-04-03 08:44:25 +0530

    Update studentmanager.js

--- Day4/studentmanager.js
+++ Day4/studentmanager.js
@@ -12,12 +12,20 @@ let users=[
 
 app.get("/getuser",(req,res)=>{
   
-    
-})
+    res.json(users)
+});
 app.post("/getuser",(req,res)=>{
-    
+const newUser = req.body;
+    users.push(newUser);
+    res.json({message: "user added:",newUser})
 })
 app.delete("/deleteuser",(req,res)=>{
+const name=req.body;
+let index=users.Indexof(name);
+if(index==-1){
+users.splice(index,1);
+res.json({message: "user deleted:"})
+}
     
 })
 
