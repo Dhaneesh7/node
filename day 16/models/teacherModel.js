5ce74797b6a5d827444a9aad33bc53aa65c87e8e
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-04-28 19:45:00 +0530

    Create day 16/models/teacherModel.js

---
+++ day 16/models/teacherModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const teacherSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+   // match: /^[A-Z\s]+$/ // Capital letters only
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 21,
+    max: 65
+  },
+  salary: {
+    type: Number,
+    required: true,
+    min: 10000,
+    max: 100000
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ['male', 'female']
+  },
+  place: {
+    type: String,
+    required: true
+  },
+  maritalStatus: {
+    type: String,
+    enum: ['married', 'unmarried'],
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    //match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+  },
+  phone: {
+    type: String,
+    required: true,
+  //  match: /^[0-9]{10}$/
+  }
+});
+
+module.exports = mongoose.model('Teacher', teacherSchema);
