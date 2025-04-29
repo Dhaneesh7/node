aea84a194f281400aa441fdc95b7babe78bd16a8
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-04-29 19:24:53 +0530

    Update teachercontrollers.js

--- day 16/controllers/teachercontrollers.js
+++ day 16/controllers/teachercontrollers.js
@@ -4,5 +4,27 @@ const newteacher= new teacher({req.body})
 }
 function getTeachers(req,res){
 
+const teachers = Teacher.find();
+    res.json(teachers);
+}
+function getTeacherMarried(req,res){
+
+const teachers =  Teacher.find({ maritalStatus: 'married' });
+  res.json({ count: teachers.length, teachers });
+}
+function getTeacherUnmarried(req,res){
+
+const teachers =  Teacher.find({ maritalStatus: 'unmarried' });
+  res.json({ count: teachers.length, teachers });
+}
+function getTeacherByPlace(req,res){
+const { place } = req.params;
+  const teachers = await Teacher.find({ place });
+  res.json(teachers);
+
+}
+function deleteTeacherById(req,res){
+Teacher.findByIdAndDelete(req.params.id);
+  res.json({ message: "Teacher deleted successfully" });
 
 }
