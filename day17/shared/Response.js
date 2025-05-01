08ccfdfbd821e2ae8e7d3f26d0ba13d696a9e01c
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-05-01 14:37:07 +0530

    Create Response.js

---
+++ day17/shared/Response.js
@@ -0,0 +1,16 @@
+class Response {
+  payload=[];
+  success=false;
+  message:'';
+setPayload(payload){
+this.payload=payload;
+}
+setSuccess(success){
+this.success=success;
+}
+ setMessage(message){
+this.message=message;
+}
+}
+
+module.exports = Response;
