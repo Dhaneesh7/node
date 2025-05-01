4434fead8b7dac4b2277d08511ce895f566aef4f
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-05-01 13:39:35 +0530

    Create day17/shared/constants.js

---
+++ day17/shared/constants.js
@@ -0,0 +1,13 @@
+const CONSTANTS ={
+PORT: 3000,
+URLS:{
+USERS PREFIX:'/users',
+TEACHERS_PREFIX: '/teachers',
+GET_ALL_USERS: '/getAllUsers',
+}
+
+mongoDBUrl:'mongodb://localhost:",
+mongoDBPort:27817,
+mongodbDBName:'/student',
+}
+module.exports = CONSTANT;
