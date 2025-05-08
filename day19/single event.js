7d2d64196f938bab7471c5a5951a161f72cdafac
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-05-08 12:07:08 +0530

    Create single event.js

---
+++ day19/single event.js
@@ -0,0 +1,13 @@
+
+const EventEmitter = require('events');
+
+
+const emitter = new EventEmitter();
+
+
+emitter.on('dataReceived', (data) => {
+  console.log('Received data:', data);
+});
+
+
+emitter.emit('dataReceived', "hello world");
